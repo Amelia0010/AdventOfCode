0e9addab3dfe3dd32130a0ecfdf528312a52ba32
Amalie Pihl <pihlamalie@gmail.com>
2025-12-12 15:37:03 -0600

    minimal haskell setup

---
+++ 2025/app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+import DayOne
+
+runDay :: String -> IO ()
+runDay "1" = DayOne.run
+runDay day = putStrLn ("Invalid Day: " ++ day)
+
+main :: IO ()
+main = do
+    putStrLn "Please choose a day from [1]"
+    day <- getLine
+    runDay day
+    

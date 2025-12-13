4fd1ba2f9f111efc3c5367e305aa9d69b6ae985f
Amalie Pihl <pihlamalie@gmail.com>
2025-12-12 22:51:13 -0600

    convert day 1 part 1 to haskell

--- 2025/app/DayOne.hs
+++ 2025/app/DayOne.hs
@@ -3,4 +3,25 @@ module DayOne where
 calculate :: IO ()
 calculate = do
     contents <- readFile "1.txt"
-    putStrLn contents
+    putStrLn (show (calculateZeroCount (lines contents) 50))
+
+calculateZeroCount :: [String] -> Int -> Int
+calculateZeroCount [] _ = 0
+calculateZeroCount operations dial
+    | nextDial == 0 = recursiveZeros + 1
+    | otherwise = recursiveZeros
+    where
+        operation = head operations
+        nextDial = calculateNextDial operation dial
+        recursiveZeros = calculateZeroCount (tail operations) nextDial
+
+
+calculateNextDial :: String -> Int -> Int
+calculateNextDial operation dial =
+    (dial + trueAngle) `mod` 100 
+    where 
+        direction = head operation
+        angle = read (tail operation)
+        trueAngle
+            | direction == 'R' = angle
+            | otherwise = -angle
